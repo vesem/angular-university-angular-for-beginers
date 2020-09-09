f6763920f8b3f75b926730196d472144dce12370
vesem <vesem@pop-os.localdomain>
2020-09-09 12:08:38 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+
+  data = {
+    title: 'Angular Core Deep Dive'
+  };
+
+  onLogoClicked() {
+    alert('Hello World');
+  }
+
+  onKeyUp(newTitle:string) {
+    this.data.title = newTitle;
+  }
+  courseTitle = 'Angular Core Deep Dive';
 }
